b273bdf50c3b28332bb3ee4f6396ea996f190552
Ville <e2301777@edu.vamk.fi>
2025-03-27 11:29:27 +0200

    income/expense decision made

--- budget-tracker/src/components/Transaction.jsx
+++ budget-tracker/src/components/Transaction.jsx
@@ -1,6 +1,6 @@
 export default function Transaction({ reason, sum, onRemove }){
     return(
-        <li className="income">
+        <li className={sum > 0 ? 'income' : 'expense'}>
             {reason}
             {sum}
             <button onClick={onRemove}>Poista</button>
